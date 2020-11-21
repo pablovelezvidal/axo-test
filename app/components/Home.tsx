b8255724af505690e6e97477b6e60ef2462f3a89
Pablo Velez <pvelez@bp-3.com>
2020-11-21 13:58:11 +0100

    Add the user search to the main page and clean up the counter comp

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,13 +1,74 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import styles from './Home.css';
+import routes from '../constants/routes.json';
 
-export default function Home(): JSX.Element {
+import { AsyncTypeahead } from 'react-bootstrap-typeahead';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+
+const SEARCH_URI =
+  'https://typeahead-js-twitter-api-proxy.herokuapp.com/demo/search';
+
+const Home = (): JSX.Element => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [options, setOptions] = useState([]);
+
+  const handleSearch = (query: any) => {
+    setIsLoading(true);
+
+    fetch(`${SEARCH_URI}?q=${query}`)
+      .then((resp) => resp.json())
+      .then((items) => {
+        console.log(items);
+        const options = items.map((i: any) => {
+          console.log(i);
+          return {
+            profile_image_url_https: i.profile_image_url_https,
+            id: i.id,
+            screen_name: i.screen_name,
+          };
+        });
+
+        setOptions(options);
+        setIsLoading(false);
+      });
+  };
+
+  // Bypass client-side filtering by returning `true`. Results are already
+  // filtered by the search endpoint, so no need to do it again.
+  const filterBy = () => true;
+
   return (
-    <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>To Twitter Search</Link>
-    </div>
+    <>
+      <div>
+        <p>Enter the Twitter User Name</p>
+      </div>
+      <AsyncTypeahead
+        filterBy={filterBy}
+        id="async-example"
+        isLoading={isLoading}
+        labelKey="screen_name"
+        minLength={3}
+        onSearch={handleSearch}
+        options={options}
+        placeholder="Search for a Github user..."
+        renderMenuItemChildren={(option, props) => (
+          <>
+            <img
+              alt={option.screen_name}
+              src={option.profile_image_url_https}
+              style={{
+                height: '24px',
+                marginRight: '10px',
+                width: '24px',
+              }}
+            />
+            <span>{option.screen_name}</span>
+          </>
+        )}
+      />
+    </>
   );
-}
+};
+
+export default Home;
--- app/features/counter/Counter.tsx
+++ app/features/counter/Counter.tsx
@@ -1,43 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Counter.css';
 import routes from '../../constants/routes.json';
 
-import { AsyncTypeahead } from 'react-bootstrap-typeahead';
-import 'react-bootstrap-typeahead/css/Typeahead.css';
-
-const SEARCH_URI =
-  'https://typeahead-js-twitter-api-proxy.herokuapp.com/demo/search';
-
 const Counter = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [options, setOptions] = useState([]);
-
-  const handleSearch = (query: any) => {
-    setIsLoading(true);
-
-    fetch(`${SEARCH_URI}?q=${query}`)
-      .then((resp) => resp.json())
-      .then((items) => {
-        console.log(items);
-        const options = items.map((i: any) => {
-          console.log(i);
-          return {
-            profile_image_url_https: i.profile_image_url_https,
-            id: i.id,
-            screen_name: i.screen_name,
-          };
-        });
-
-        setOptions(options);
-        setIsLoading(false);
-      });
-  };
-
-  // Bypass client-side filtering by returning `true`. Results are already
-  // filtered by the search endpoint, so no need to do it again.
-  const filterBy = () => true;
-
   return (
     <>
       <div className={styles.backButton} data-tid="backButton">
@@ -45,30 +11,7 @@ const Counter = () => {
           <i className="fa fa-arrow-left fa-3x" />
         </Link>
       </div>
-      <AsyncTypeahead
-        filterBy={filterBy}
-        id="async-example"
-        isLoading={isLoading}
-        labelKey="screen_name"
-        minLength={3}
-        onSearch={handleSearch}
-        options={options}
-        placeholder="Search for a Github user..."
-        renderMenuItemChildren={(option, props) => (
-          <>
-            <img
-              alt={option.screen_name}
-              src={option.profile_image_url_https}
-              style={{
-                height: '24px',
-                marginRight: '10px',
-                width: '24px',
-              }}
-            />
-            <span>{option.screen_name}</span>
-          </>
-        )}
-      />
+      <p>Tweets from the user : USER</p>
     </>
   );
 };
